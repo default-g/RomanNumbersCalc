c397aa995aa9320245c909acc4744c6090b667e4
Artem <arteam_101@mail.ru>
2022-03-04 18:06:28 +0700

    Первые бинды на кнопках

--- AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Reactive;
+using ReactiveUI;
 
 namespace AvaloniaApplication1.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        string input = "";
+        public MainWindowViewModel()
+        {
+            ChangeInput = ReactiveCommand.Create((string x) => Input += x);
+
+        }
+        public string Input
+        {
+            set
+            {
+                this.RaiseAndSetIfChanged(ref input, value);
+            }
+            get
+            {
+                return this.input;
+            }
+        }
+        public ReactiveCommand<string, string> ChangeInput { get; }
+
     }
 }
